712448e55957ef1132b6b49804e3927201ee1200
PiotrCyganik <pi.cyganik@gmail.com>
2019-06-25 18:56:35 +0200

    Refactored

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-
+ before_action :set_todo, only: [:edit, :update, :show, :destroy]
   def new
     @todo = Todo.new
   end
@@ -17,15 +17,15 @@ class TodosController < ApplicationController
   end
 
   def show
-    @todo = Todo.find(params[:id])
+
   end
 
   def edit
-    @todo = Todo.find(params[:id])
+    
   end
 
   def update
-    @todo = Todo.find(params[:id])
+
     if @todo.update(todo_params)
       flash[:notice] = "Todo successfilly updated"
       redirect_to todo_path(@todo)
@@ -40,13 +40,17 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo = Todo.find(params[:id])
+
     @todo.destroy
     flash[:notice] = "Item deleted successfully"
     redirect_to todos_path
   end
 
   private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 
     def todo_params
       params.require(:todo).permit(:name , :description)
